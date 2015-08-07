1a2000c4d3524e3d73be3a2a12a0d1fcf8feb608
Joffrmann29 <Joffrmann25@gmail.com>
2015-08-07 09:19:00 -0400

    Initial Commit

---
+++ TrumpGram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrumpGram
+//
+//  Created by Joffrey Mann on 8/7/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
